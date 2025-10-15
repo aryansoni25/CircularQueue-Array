df307f326c46a85c465d07b9e4e21a060eab5528
aryansoni25 <ashoksoni9425@gmail.com>
2025-10-15 08:23:28 +0530

    some changes

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -2,13 +2,11 @@ package org.studyeasy;
 
 public class Main {
     public static void main(String[] args) {
-        CircularQueue cq=new CircularQueue(6);
+        CircularQueue cq=new CircularQueue(4);
         cq.EnQueue(10);
         cq.EnQueue(20);
         cq.EnQueue(30);
         cq.EnQueue(40);
-        cq.EnQueue(50);
-        cq.EnQueue(60);
         System.out.println(cq.DeQueue());
         System.out.println(cq.peek());
     }
